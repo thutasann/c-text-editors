cda513bc1a20d72c068fdb27fce522628bc80db2
thutasann <thutasann2002@gmail.com>
2024-10-23 20:48:32 +0800

    fix: backspace

--- src/first-editor/main.c
+++ src/first-editor/main.c
@@ -7,24 +7,33 @@ void welcome_print(const char *message);
     ## First Text Editor
  */
 int main(void) {
-    initscr(); // init
-
-    start_color();                          // start color functionality
-    init_pair(1, COLOR_GREEN, COLOR_BLACK); // (foreground, background)
-
+    initscr();            // init
     raw();                // line buffering disabled
     keypad(stdscr, TRUE); // get F1, F2, etc...
     noecho();             // dont echo() while we do getch
 
+    start_color();                          // start color functionality
+    init_pair(1, COLOR_GREEN, COLOR_BLACK); // (foreground, background)
+
     int row, col;
     getmaxyx(stdscr, row, col);
-    mvprintw(row - 1, col, "NORMAL");
+
+    mvprintw(row - 1, 0, "NORMAL");
     move(0, 0);
 
     int ch = getch();
+    addch(ch);
+
     while (ch != 'q') {
-        addch(ch);
         ch = getch();
+        if (ch == 127) {
+            int x, y;
+            getyx(stdscr, y, x);
+            move(y, x - 1);
+            delch();
+        } else {
+            addch(ch);
+        }
     }
 
     refresh(); // print it on the real screen
