f38c500880cc05e3e9580381724550d7ba1ee039
thutasann <thutasann2002@gmail.com>
2024-11-09 15:12:08 +0800

    tweak: http_response

--- src/web_server_from_scratch/Main.c
+++ src/web_server_from_scratch/Main.c
@@ -14,14 +14,11 @@ void launch(struct Server *server) {
 
     // http response
     const char *http_response =
-        "HTTP/1.1 200 OK\r\n"
-        "Date: Mon, 27 Jul 2024 12:28:53 GMT\r\n"
-        "Server: Apache/2.2.14 (Win32)\r\n"
-        "Last-Modified: Wed, 22 Jul 2024 19:15:56 GMT\r\n"
-        "Content-Length: 88\r\n"
-        "Content-Type: text/html\r\n"
-        "Connection: close\r\n"
-        "\r\n"
+        "HTTP/1.1 200 OK\nGMT\n"
+        "Server: Apache/2.2.14 (Win32)\n"
+        "Last-Modified: Wed, 22 Jul 2024 19:15:56 GMT\n"
+        "Content-Type: text/html\n"
+        "Connection: Closed\n\n"
         "<html>"
         "<body>"
         "<h1>HelloWorld</h1>"
